b7a21c0234b31abc25429159e15f553802d626d3
Fatima <fatergesh@gmail.com>
2025-02-04 16:10:02 +0600

    added style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,8 @@ import meal from '../assets/image/meal.png'
 import animation from '../assets/image/coffee.png'
 import online from '../assets/image/online.png'
 
+import { Link } from 'react-router-dom'
+
 function Projects() {
   return (
     <div className='projects-my'>
@@ -13,57 +15,45 @@ function Projects() {
       <h3>My Projects</h3>
       <div className='dash3'></div>
       </div>
-
       <div className='projects-containers'>
-
-
+      <Link to="https://meal-food.vercel.app/">
       <div className='project'>
-          <a href="https://meal-food.vercel.app/">
           <img src={meal} alt="" />
-          </a>
           <h5>TheMealDB</h5>
           <p>This project is designed for searching and ordering food.</p>
         </div>
-
-
+      </Link>
+        <Link to="https://minute-ten.vercel.app/">
         <div className='project'>
-          <a href="https://minute-ten.vercel.app/">
           <img src={minute} alt="" />
-          </a>
           <h5>Hours, minutes, seconds</h5>
           <p>“This project calculates hours, minutes, and seconds.”</p>
         </div>
-
-        <div className='project'>
-          <a href="https://animations-3604pqcdd-fatimaervs-projects.vercel.app/">
-          <img src={animation} alt="" />
-          </a>
+        </Link>
+         <Link to="https://animations-3604pqcdd-fatimaervs-projects.vercel.app/">
+         <div className='project'>
+          <img src={animation} alt="Animation" />
           <h5>Animations</h5>
           <p>“This project is an animated coffee.”</p>
         </div>
-
+         </Link>
+        <Link to="https://online-shop-sand-five.vercel.app/">
         <div className='project'>
-          <a  href="https://online-shop-sand-five.vercel.app/">
-          <img src={online} alt="" />
-          </a>
-          <h5>Online-Shop</h5>
-          <p>This project helps to purchase online,</p>
-          <p> providing convenience for each individual  </p>
-        </div>
-        <div className='project'>
-          <a href="https://github.com/Asel604/finaly-project">
+        <img src={online} alt="" />
+        <h5>Online-Shop</h5>
+        <p>This project helps to purchase online, providing convenience for each individual </p>
+       
+      </div>
+        </Link>
+       <Link to="https://github.com/Asel604/finaly-project">
+       <div className='project'>
           <img src={bath} alt="" />
-          </a>
           <h5>Bathroom Mebels</h5>
           <p>This project helps to purchase furniture and accessories for bathrooms and showers online.</p>
         </div>
-
+       </Link>
       </div>
-
-      
-
-      <div>
-        
+      <div>  
       </div>
     </div>
   )
